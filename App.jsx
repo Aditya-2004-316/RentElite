39bd5038a41fef05b0286dea3b59afe7bed1e294
Aditya Vishwakarma <165813206+Aditya-2004-316@users.noreply.github.com>
2025-01-11 15:49:29 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,28 +4,34 @@ import SignUp from "./components/SignUp"; // Import the SignUp component
 import ForgotPassword from "./components/ForgotPassword"; // Import the ForgotPassword component
 import Dashboard from "./components/Dashboard"; // Import the Dashboard component
 import Profile from "./components/Profile";
+import { BookingProvider } from "./context/BookingContext";
+import MyBookings from "./components/MyBookings";
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />{" "}
-                {/* Set Login as the default route */}
-                <Route path="/signup" element={<SignUp />} />{" "}
-                {/* Add SignUp route */}
-                <Route
-                    path="/forgot-password"
-                    element={<ForgotPassword />}
-                />{" "}
-                {/* Add Forgot Password route */}
-                <Route path="/dashboard" element={<Dashboard />} />{" "}
-                {/* Add Dashboard route */}
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
-        </Router>
+        <BookingProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login />} />{" "}
+                    {/* Set Login as the default route */}
+                    <Route path="/signup" element={<SignUp />} />{" "}
+                    {/* Add SignUp route */}
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />{" "}
+                    {/* Add Forgot Password route */}
+                    <Route path="/dashboard" element={<Dashboard />} />{" "}
+                    {/* Add Dashboard route */}
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/my-bookings" element={<MyBookings />} />
+                </Routes>
+            </Router>
+        </BookingProvider>
     );
 };
 
 export default App;
+
 
 
