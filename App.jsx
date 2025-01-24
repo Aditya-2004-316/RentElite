0e1f06e0667e64263dcb52a0d95e54ca51b11482
Aditya Vishwakarma <165813206+Aditya-2004-316@users.noreply.github.com>
2025-01-25 01:52:35 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login"; // Ensure this import matches your file structure
 import SignUp from "./components/SignUp"; // Import the SignUp component
@@ -6,6 +7,12 @@ import Dashboard from "./components/Dashboard"; // Import the Dashboard componen
 import Profile from "./components/Profile";
 import { BookingProvider } from "./context/BookingContext";
 import MyBookings from "./components/MyBookings";
+import PrivacyPolicy from "./components/PrivacyPolicy"; // Import the PrivacyPolicy component
+import TermsOfService from "./components/TermsOfService";
+import ContactUs from "./components/ContactUs"; // Import the TermsOfService component
+import AboutUs from "./components/AboutUs";
+import FAQs from "./components/FAQs";
+import Support from "./components/Support";
 
 const App = () => {
     return (
@@ -25,6 +32,15 @@ const App = () => {
                     {/* Add Dashboard route */}
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/my-bookings" element={<MyBookings />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route
+                        path="/terms-of-service"
+                        element={<TermsOfService />}
+                    />
+                    <Route path="/contact-us" element={<ContactUs />} />
+                    <Route path="/about-us" element={<AboutUs />} />
+                    <Route path="/faqs" element={<FAQs />} />
+                    <Route path="/support" element={<Support />} />
                 </Routes>
             </Router>
         </BookingProvider>
@@ -32,6 +48,7 @@ const App = () => {
 };
 
 export default App;
+
 
 
 
